15f2d8db748af61d6020b9937b044bd96c95c220
Nina Galagan <kler.mongomeri.9851@mail.ru>
2022-12-30 17:12:57 +0300

    Добавление папок

---
+++ src/main/groovy/Main.java
@@ -0,0 +1,53 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+
+
+
+public class Main {
+    private static int upvotes;
+
+    public static void main(String ... args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+        HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+        CloseableHttpResponse response = httpClient.execute(request);
+        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+        String body = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
+        httpClient.close();
+        readAnswer(body);
+    }
+
+        static void readAnswer(String body) throws JsonProcessingException {
+            ObjectMapper mapper = new ObjectMapper();
+
+            List<Cat> cats = mapper.readValue(body, new TypeReference<>(){});
+                    cats.stream()
+                    .filter(ri -> ri.getUpvotes()!=0)
+                    .forEach(System.out::println);
+
+            }
+
+
+}
+
+
